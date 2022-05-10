441ba243f427de1f0afe193eadd28db0eb92da8d
zhSHUVO <zhshuvo99@gmail.com>
2022-05-11 02:50:22 +0600

    info card

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React from "react";
+import clock from "../../assets/icons/clock.svg";
+import marker from "../../assets/icons/marker.svg";
+import phone from "../../assets/icons/phone.svg";
 
 const Info = () => {
     return (
-        <div>
-            
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 px-12">
+            <div class="card lg:card-side bg-base-100 shadow-xl bg-primary text-white px-8 pt-6">
+                <figure>
+                    <img src={clock} alt="opening hour" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">Opening Hours</h2>
+                    <p>Lorem Ipsum is simply dummy text of the pri</p>
+                </div>
+            </div>
+            <div class="card lg:card-side bg-base-100 shadow-xl bg-accent text-white px-8 pt-6">
+                <figure>
+                    <img src={marker} alt="location marker" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">Visit our location</h2>
+                    <p>Brooklyn, NY 10036, United States</p>
+                </div>
+            </div>
+            <div class="card lg:card-side bg-base-100 shadow-xl bg-primary text-white px-8 pt-6">
+                <figure>
+                    <img src={phone} alt="phone" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">Contact us now</h2>
+                    <p>+000 123 456789</p>
+                </div>
+            </div>
         </div>
     );
 };
