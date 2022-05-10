cc5965028583296788a6a6642e843ee598fd6704
zhSHUVO <zhshuvo99@gmail.com>
2022-05-10 19:43:10 +0600

    nav and banner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import About from "./Pages/About/About";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
 import Navbar from "./Pages/Shared/Navbar";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="about" element={<About />} />
+                <Route path="login" element={<Login />} />
             </Routes>
         </div>
     );
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
@@ -26,71 +27,60 @@ const Navbar = () => {
                         class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
                     >
                         <li>
-                            <a>Item 1</a>
+                            <Link to="/">Home</Link>
                         </li>
-                        <li tabindex="0">
-                            <a class="justify-between">
-                                Parent
-                                <svg
-                                    class="fill-current"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                                </svg>
-                            </a>
-                            <ul class="p-2">
-                                <li>
-                                    <a>Submenu 1</a>
-                                </li>
-                                <li>
-                                    <a>Submenu 2</a>
-                                </li>
-                            </ul>
+
+                        <li>
+                            {" "}
+                            <Link to="/about">About</Link>
                         </li>
                         <li>
-                            <a>Item 3</a>
+                            {" "}
+                            <Link to="/appointment">Appointment</Link>
+                        </li>
+                        <li>
+                            {" "}
+                            <Link to="/review">Reviews</Link>
+                        </li>
+                        <li>
+                            {" "}
+                            <Link to="/contact">Contact Us</Link>
+                        </li>
+                        <li>
+                            {" "}
+                            <Link to="/login">Login</Link>
                         </li>
                     </ul>
                 </div>
-                <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <a class="btn btn-ghost normal-case text-xl">Doctors Portal</a>
             </div>
             <div class="navbar-center hidden lg:flex">
                 <ul class="menu menu-horizontal p-0">
                     <li>
-                        <a>Item 1</a>
+                        <Link to="/">Home</Link>
                     </li>
-                    <li tabindex="0">
-                        <a>
-                            Parent
-                            <svg
-                                class="fill-current"
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="20"
-                                height="20"
-                                viewBox="0 0 24 24"
-                            >
-                                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                            </svg>
-                        </a>
-                        <ul class="p-2">
-                            <li>
-                                <a>Submenu 1</a>
-                            </li>
-                            <li>
-                                <a>Submenu 2</a>
-                            </li>
-                        </ul>
+
+                    <li>
+                        {" "}
+                        <Link to="/about">About</Link>
                     </li>
                     <li>
-                        <a>Item 3</a>
+                        {" "}
+                        <Link to="/appointment">Appointment</Link>
+                    </li>
+                    <li>
+                        {" "}
+                        <Link to="/review">Reviews</Link>
+                    </li>
+                    <li>
+                        {" "}
+                        <Link to="/contact">Contact Us</Link>
+                    </li>
+                    <li>
+                        {" "}
+                        <Link to="/login">Login</Link>
                     </li>
                 </ul>
-            </div>
-            <div class="navbar-end">
-                <a class="btn">Get started</a>
             </div>
         </div>
     );
